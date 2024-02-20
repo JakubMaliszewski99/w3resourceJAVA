eeb3bc7ce09e732ae44416f1648d2fb6a316bfcc
JakubMaliszewski99 <kubek.mal@gmail.com>
2024-02-20 13:49:33 +0100

    Ex 23 modified and created a Class for conversions

--- Basics/23/Main.java
+++ Basics/23/Main.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main{
@@ -15,33 +17,26 @@ public class Main{
         long binNum = 0;
         int dec = 0;
         char[] hex = new char[20];
-        char[] hexAl = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-        byte i = 0;
+        int hexLength = 0;
 
         // Take input
         System.out.print("Input Binary Number: ");
         binNum = scanner.nextLong();
 
         // Convert bin to dec
-        while (binNum != 0){
-            dec += binNum % 2 * Math.pow(2, i++);
-            binNum /= 10;
-        }
+        dec = Conversions.convBin2Dec(binNum);
 
-        i = 0;
+        // Convert decimal to hex (you get the digits in a reversed order)
+        hex = Conversions.convDec2Hex(dec);
 
-        // Convert decimal to hex
-        while (dec != 0){
-            hex[i++] = hexAl[dec % 16];
-            dec /= 16; 
+        for (int j = 0; hex[j] != 0; j++){
+            hexLength++;
         }
 
         System.out.print("Hexadecimal Number: ");
-        i--;
-        while (i>=0){
-            System.out.print(hex[i--]);
+        for (int i = hexLength; i >= 0; i--){
+        System.out.print(hex[i]);
         }
-
         scanner.close();
     }
 }
